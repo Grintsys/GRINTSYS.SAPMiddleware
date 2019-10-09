119e2b8547927563ff23d6372a55bdb51892b08b
Sergio Peralta <serpel.js@gmail.com>
2019-10-08 20:36:10 -0600

    lastest migration

---
+++ src/GRINTSYS.SAPMiddleware.EntityFrameworkCore/Migrations/20191009023508_AddedCardNameOnOrderModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRINTSYS.SAPMiddleware.Migrations
+{
+    public partial class AddedCardNameOnOrderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CardName",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardName",
+                table: "Orders");
+        }
+    }
+}
